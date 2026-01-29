b80c40c92fab756bc8bc44bbecb53e4213363a21
Kacper Zdanowicz <zdanowicz@outlook.com>
2026-01-29 14:02:15 +0100

    Change #D scene perspective to 1200px

--- components/inventory/weapon-inventory.tsx
+++ components/inventory/weapon-inventory.tsx
@@ -97,7 +97,7 @@ export function WeaponInventory() {
       <div 
         className="flex-1 flex items-center justify-center p-4"
         style={{ 
-          perspective: '1800px',
+          perspective: '1200px',
           perspectiveOrigin: '50% 45%',
         }}
       >
